1be61c2647f667d2395eedecce63c80bae52c54b
apple <apple@lixiang-mac.local>
2014-03-25 18:51:11 +0800

    Initial Commit

---
+++ coyote-phy/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  coyote-phy
+//
+//  Created by apple on 14-3-25.
+//  Copyright (c) 2014年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ coyote-phyTests/coyote_phyTests.m
@@ -0,0 +1,34 @@
+//
+//  coyote_phyTests.m
+//  coyote-phyTests
+//
+//  Created by apple on 14-3-25.
+//  Copyright (c) 2014年 apple. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface coyote_phyTests : XCTestCase
+
+@end
+
+@implementation coyote_phyTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
